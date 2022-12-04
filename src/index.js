5c09a65f2f7bcad7953a1a7a7f01f53a3afe4150
jovan <jovan_civkoski@hotmail.com>
2022-12-04 20:46:04 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App'
+import List from "./List"
+
+ReactDOM.render(
+  <>
+    <App />
+    <List />
+  </>,
+  document.getElementById('root')
+);
